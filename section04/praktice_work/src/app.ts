9ee53baded6b4e65957b1de07b7db664e794b3da
Vitalii Storozh <sv301185@gmail.com>
2022-12-03 11:52:24 +0200

    New features in TS

--- section04/praktice_work/src/app.ts
+++ section04/praktice_work/src/app.ts
@@ -1,44 +1,57 @@
-const userName = "BadMax";
-// userName = "Miki";
-let age = 30;
+// const userName = "BadMax";
+// // userName = "Miki";
+// let age = 30;
 
-age = 29;
+// age = 29;
 
-// function add(a: number, b: number) {
-//   let result;
-//   result = a + b;
-//   return result;
-// }
+// // function add(a: number, b: number) {
+// //   let result;
+// //   result = a + b;
+// //   return result;
+// // }
 
-// if (age > 20) {
-//   let isOld = true;
-// }
+// // if (age > 20) {
+// //   let isOld = true;
+// // }
 
-// console.log(isOld);
+// // console.log(isOld);
 
-const add = (a: number, b: number = 1) => a + b;
-console.log(add(2, 3));
+// // const add = (a: number, b: number = 1) => a + b;
+// // console.log(add(2, 3));
 
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output);
+// // const printOutput: (a: number | string) => void = (output) =>
+// //   console.log(output);
 
-const button = document.querySelector("button");
+// // const button = document.querySelector("button");
 
-if (button) {
-  button.addEventListener("click", (event) => console.log(event));
-}
+// // if (button) {
+// //   button.addEventListener("click", (event) => console.log(event));
+// // }
 
-printOutput(add(2));
+// // printOutput(add(2));
 
-const hobbies = ["Sport", "Cooking"];
+// const hobbies = ["Sport", "Cooking"];
 
-const activeHobbies = ["Hiking"];
+// const activeHobbies = ["Hiking"];
 
-activeHobbies.push(...hobbies);
+// activeHobbies.push(...hobbies);
 
-const person = {
-  name: "Max",
-  age: 30,
+// const person = {
+//   name: "Max",
+//   age: 30,
+// };
+
+// const copiedPerson = { ...person };
+
+// const add = (...numbers: number[]) => {
+//   return numbers.reduce((curResult, curValue) => {
+//     return curResult + curValue;
+//   }, 0);
+// };
+
+// const addedNumbers = add(5, 4, 20, 23, 7)
+// console.log(addedNumbers)
+
+const greetUser = (): void => {
+  alert("Hello, nice to see you!");
 };
-
-const copiedPerson = { ...person };
