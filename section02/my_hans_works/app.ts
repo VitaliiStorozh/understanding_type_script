591a2b85efd5e3e2fdf1da859008365596c505c2
Vitalii Storozh <sv301185@gmail.com>
2023-04-22 12:20:56 +0300

    refactored basic information

--- section02/my_hans_works/app.ts
+++ section02/my_hans_works/app.ts
@@ -1,26 +1,18 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
-
-function printResult(num: number) {
-  console.log("Result: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('Incorrect input!');
+  // }
   const result = n1 + n2;
-  cb(result);
+  if (showResult) {
+    console.log(phrase, result);
+  } else {
+    return result;
+  }
 }
 
-printResult(add(5, 12));
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+add(number1, number2, printResult, resultPhrase);
