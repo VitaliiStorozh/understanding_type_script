f1b4d885e68b1e381cfb4738a747913f51d41433
Vitalii Storozh <sv301185@gmail.com>
2023-04-23 15:22:40 +0300

    Working with Enums

--- section02/my_hans_works/app.ts
+++ section02/my_hans_works/app.ts
@@ -2,11 +2,39 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Vitalii',
+//   age: 37,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: 'Vitalii',
   age: 37,
   hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user']
+console.log(person.role);
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -15,4 +43,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby);
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is admin');
 }
