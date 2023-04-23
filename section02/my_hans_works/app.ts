3d54990fdaacaebc94700277de19c135b182ae39
Vitalii Storozh <sv301185@gmail.com>
2023-04-23 22:23:53 +0300

    The "unknown" Type

--- section02/my_hans_works/app.ts
+++ section02/my_hans_works/app.ts
@@ -1,26 +1,10 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+let userInput: unknown;
+let userName: string;
 
-function printResult(num: number) {
-  console.log('Result: ' + num);
-}
+userInput = 5;
+userInput = 'Max';
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult; // Error
-// combineValues = 5; // Error
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, result => {
-  console.log(result);
-});
+// userName = userInput; // Error
+if (typeof userInput === 'string') {
+  userName = userInput;
+} // Type guard
