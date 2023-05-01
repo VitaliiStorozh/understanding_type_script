f71fc0b051c4be7a57f49d172bb4d5d1883c0dd5
Vitalii Storozh <sv301185@gmail.com>
2023-05-01 09:35:42 +0300

    Modern JS features vs TS

--- section04/praktice_work/src/app.ts
+++ section04/praktice_work/src/app.ts
@@ -1,57 +1,28 @@
-// const userName = "BadMax";
-// // userName = "Miki";
-// let age = 30;
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking', ...hobbies];
 
-// age = 29;
+activeHobbies.push(...hobbies);
 
-// // function add(a: number, b: number) {
-// //   let result;
-// //   result = a + b;
-// //   return result;
-// // }
-
-// // if (age > 20) {
-// //   let isOld = true;
-// // }
-
-// // console.log(isOld);
-
-// // const add = (a: number, b: number = 1) => a + b;
-// // console.log(add(2, 3));
-
-// // const printOutput: (a: number | string) => void = (output) =>
-// //   console.log(output);
-
-// // const button = document.querySelector("button");
-
-// // if (button) {
-// //   button.addEventListener("click", (event) => console.log(event));
-// // }
-
-// // printOutput(add(2));
-
-// const hobbies = ["Sport", "Cooking"];
-
-// const activeHobbies = ["Hiking"];
-
-// activeHobbies.push(...hobbies);
-
-// const person = {
-//   name: "Max",
-//   age: 30,
-// };
-
-// const copiedPerson = { ...person };
-
-// const add = (...numbers: number[]) => {
-//   return numbers.reduce((curResult, curValue) => {
-//     return curResult + curValue;
-//   }, 0);
-// };
-
-// const addedNumbers = add(5, 4, 20, 23, 7)
-// console.log(addedNumbers)
-
-const greetUser = (): void => {
-  alert("Hello, nice to see you!");
+const person = {
+  firstName: 'Vit',
+  age: 37,
 };
+
+const copiedPerson = { ...person };
+console.log(person, copiedPerson);
+
+const add = (...numbers: number[]) =>
+  numbers.reduce((curResult, curValue) => curResult + curValue, 0);
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobby1, hobby2, hobbies);
+
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
