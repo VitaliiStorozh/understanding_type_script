9ffa77f66557ec9a8cf90cbddb95202fd4396b90
Vitalii Storozh <sv301185@gmail.com>
2023-05-06 16:20:04 +0300

    simple class creating

--- section05/praktice_work/src/app.ts
+++ section05/praktice_work/src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
